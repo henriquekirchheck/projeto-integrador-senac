396828df4e8c87a1fe3bf26615211454a41b9e49
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2024-09-17 16:54:13 -0300

    Initial website

---
+++ apps/client-website/eslint.config.js
@@ -0,0 +1,40 @@
+// @ts-check
+import js from "@eslint/js";
+import globals from "globals";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default tseslint.config(
+  { ignores: ["dist"] },
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+    ],
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      // @ts-ignore
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    // @ts-ignore
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      ...react.configs.recommended.rules,
+      ...react.configs["jsx-runtime"].rules,
+    },
+  },
+  eslintConfigPrettier,
+);
