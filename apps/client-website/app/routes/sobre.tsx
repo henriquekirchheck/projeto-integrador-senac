fc1081ce5dcf691fb84d30e4f175114700b327e8
GABRIEL CARAMEZ BENVINDO DA SILVA <155910430+GabrielCaramez@users.noreply.github.com>
2024-09-26 16:43:49 -0300

    ajuste no className

--- apps/client-website/app/routes/sobre.tsx
+++ apps/client-website/app/routes/sobre.tsx
@@ -9,7 +9,7 @@ export const Route = createFileRoute("/sobre")({
 function About() {
   return (
     <div className="flex p-4">
-      <div className="relative my-auto w-1/2 lg:w-1/3 lg:block hidden">
+      <div className="relative my-auto hidden w-1/2 lg:block lg:w-1/3">
         <h2 className="absolute z-10 w-full p-4 font-script text-4xl font-bold md:w-1/2 lg:w-1/3">
           Pizzas de Qualidades desde 1983
         </h2>
@@ -23,7 +23,7 @@ function About() {
         <h1 className="text-center font-script text-3xl font-semibold">
           Sobre nós
         </h1>
-        <p className="text-base md:text-lg lg:text-xl text-justify">
+        <p className="text-justify text-base md:text-lg lg:text-xl">
           Bem-vindo à Placeholder Inc., onde a paixão pela pizza se encontra com
           a criatividade! Fundada por um grupo de amigos que compartilham o amor
           pela boa comida e momentos especiais, nossa pizzaria é mais do que um
