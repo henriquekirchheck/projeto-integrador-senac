6c18211a945f180532c264bf886249d1a0592759
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2025-04-07 22:45:33 -0300

    Flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { flake-utils, nixpkgs, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = (import nixpkgs { inherit system; });
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.nodePackages.pnpm
+            pkgs.nodePackages.prettier
+            pkgs.nodePackages.eslint
+            pkgs.nodePackages.typescript
+            pkgs.nodePackages.typescript-language-server
+          ];
+        };
+      }
+    );
+}
